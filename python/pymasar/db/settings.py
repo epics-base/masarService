399184c4d02ba2f0379155812ea82e16ef2c96d9
Guobao Shen <unknown>
2012-03-26 08:53:56 -0400

    bug fixed in user manual

--- python/pymasar/db/settings.py
+++ python/pymasar/db/settings.py
@@ -11,6 +11,10 @@ and load service info, pv_group, and event configuration.
 
 # pv group name: [pv list file, description]
 pvgroups= {
+           #test
+           'test':            ['example.txt', 'server test'],
+           'wftest':          ['exampleWf.txt', 'server test with waveform'],
+           'bigwftest':       ['exampleBigWf.txt', 'server test with big waveform'],
            #LTD2_SC_Daily_All
            'LTD2_BPM_read':   ['LTD2/Read_PV/LTD2_BPM_PV_read.txt', "Linac to dump2 beam line measured aver. , std. Error and Interest shot number for x, y position and sum signal readback"],
            'LTD2_Flag_read':  ['LTD2/Read_PV/LTD2_Flag_PV_read.txt',"Linac to dump2 beam line flag's image, x/y min. start position,  centriods and rms sizes"],
@@ -45,16 +49,28 @@ pvgroups= {
 
 # config name: [config desc, system]
 configs= {
+          # test
+          'sr_test':              ['test pv config', 'test'],
+          'wf_test':              ['waveform test pv config', 'test'],
+          'bwf_test':             ['big waveform test pv config', 'test'],
+          # LTD2
           'LTD2_SC_Daily_All':    ['LTD2 daily reference',    'LTD2'],
           'LTD2_SCR_Daily_All':   ['LTD2 daily SCR setpoint', 'LTD2'],
+          # LTD1
           'LTD1_SC_Daily_All':    ['LTD1 daily reference',    'LTD1'],
           'LTD1_SCR_Daily_All':   ['LTD1 daily SCR setpoint', 'LTD1'] 
 }
 
 # config name: [pvgroup,]
 pvg2config= {
+             # test
+             'sr_test':             ['test'],
+             'wf_test':             ['wftest'],
+             'bwf_test':            ['wftest', 'bigwftest'],
+             # LTD2
              'LTD2_SC_Daily_All':   ['LTD2_BPM_read', 'LTD2_Flag_read', 'LTD2_ES_read', 'LTD2_FCT_read', 'LTD2_ICT_read', 'LTD2_FC_read'],
              'LTD2_SCR_Daily_All':  ['LTD2_BPM_set', 'LTD2_Flag_set', 'LTD2_Cor_set', 'LTD2_Quad_set', 'LTD2_Bend_set', 'LTD2_ES_set', 'LTD2_FCT_set', 'LTD2_ICT_set', 'LTD2_FC_set'],
+             # LTD1
              'LTD1_SC_Daily_All':   ['LTD1_BPM_read', 'LTD1_Flag_read', 'LTD1_ICT_read', 'LTD1_FC_read'],
              'LTD1_SCR_Daily_All':  ['LTD1_BPM_set', 'LTD1_Flag_set', 'LTD1_Cor_set', 'LTD1_Quad_set', 'LTD1_ICT_set', 'LTD1_Bend_set', 'LTD1_FC_set']
 }
