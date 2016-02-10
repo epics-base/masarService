f8bf82a79b6a42fadcd108af1af31993dcde2bb1
Michael Davidsaver <mdavidsaver@gmail.com>
2016-02-10 10:15:07 -0500

    cleanup in MasarService::request
    Translate all exceptions to RPCRequestException to avoid
    abort() when throw() specifier is violated.
    
    Move common functionName handling before connditional.
    
    Use const& to avoid vector copy of getFieldNames()

--- cpp/src/server/service/masarService.cpp
+++ cpp/src/server/service/masarService.cpp
@@ -46,62 +46,49 @@ void MasarService::destroy()
 PVStructurePtr MasarService::request(
     PVStructurePtr const & pvArgument) throw (epics::pvAccess::RPCRequestException)
 {
-    assert(dslRdb.get() != NULL);
+    try{
+        assert(dslRdb.get() != NULL);
 
-    if(!NTNameValue::is_a(pvArgument->getStructure())) {
-        // support non NTNameValue Pair for some general purpose client, for example command line tools
-        string functionName;
-        PVStringPtr pvFunction = pvArgument->getSubFieldT<PVString>("function");
-        if (!pvFunction) {
+        string functionName(pvArgument->getSubFieldT<PVString>("function")->get());
+        if(functionName.empty()) {
             throw epics::pvAccess::RPCRequestException(
-                Status::STATUSTYPE_ERROR,"unknown MASAR function");
+                        Status::STATUSTYPE_ERROR,"pvArgument has an unsupported function");
         }
-        functionName = pvFunction->get();
-        if(functionName.size() < 1) {
-            throw epics::pvAccess::RPCRequestException(
-                Status::STATUSTYPE_ERROR,"pvArgument has an unsupported function");
-        }
-        StringArray fieldNames = pvArgument->getStructure()->getFieldNames();
-        size_t fieldcounts = (size_t) fieldNames.size();
-        shared_vector<string> names  (fieldcounts-1);
-        shared_vector<string> values (fieldcounts-1);
-        size_t counts = 0;
-        for (size_t i = 0; i < fieldcounts; i ++) {
-            if(fieldNames[i].compare("function")!=0) {
-                names[counts] = fieldNames[i];
-                values[counts] = pvArgument->getSubFieldT<PVString>(fieldNames[i])->get();
-                counts += 1;
+
+        if(!NTNameValue::is_a(pvArgument->getStructure())) {
+            // support non NTNameValue Pair for some general purpose client, for example command line tools
+
+            const StringArray& fieldNames = pvArgument->getStructure()->getFieldNames();
+            size_t fieldcounts = fieldNames.size();
+            shared_vector<string> names  (fieldcounts-1);
+            shared_vector<string> values (fieldcounts-1);
+            size_t counts = 0;
+            for (size_t i = 0; i < fieldcounts; i ++) {
+                if(fieldNames[i].compare("function")!=0) {
+                    names[counts] = fieldNames[i];
+                    values[counts] = pvArgument->getSubFieldT<PVString>(fieldNames[i])->get();
+                    counts += 1;
+                }
             }
-        }
-        try {
+
             PVStructurePtr result = dslRdb->request(functionName, freeze(names), freeze(values));
             return result;
-        } catch (std::exception &e) {
-            throw RPCRequestException(Status::STATUSTYPE_ERROR,
-                std::string("request failed ") + e.what());
-        }
-    } else{
-        string functionName;
-        PVStringPtr pvFunction = pvArgument->getSubField<PVString>("function");
-        if (!pvFunction) {
-            throw epics::pvAccess::RPCRequestException(
-                 Status::STATUSTYPE_ERROR,"unknown MASAR function");
-        }
-        functionName = pvFunction->get();
-        if(functionName.size()<1) {
-            throw epics::pvAccess::RPCRequestException(
-                Status::STATUSTYPE_ERROR,"pvArgument has an unsupported function");
-        }
-        try {
+        } else{
 
             const shared_vector<const string> name = pvArgument->getSubFieldT<PVStringArray>("name")->view();
             const shared_vector<const string> value = pvArgument->getSubFieldT<PVStringArray>("value")->view();
             PVStructurePtr result = dslRdb->request(functionName, name, value);
             return result;
-        } catch (std::exception &e) {
-            throw RPCRequestException(Status::STATUSTYPE_ERROR,
-                std::string("request failed ") + e.what());
         }
+
+    }catch(epics::pvAccess::RPCRequestException&){
+        throw;
+    }catch(std::exception& e){
+        // since request(RPCRequestException) in our base class has a throw() specifier,
+        // if anything else is thrown we abort() :P
+        // so must translate all exceptions to RPCRequestException.
+        throw epics::pvAccess::RPCRequestException(Status::STATUSTYPE_ERROR,
+                                                   std::string("request failed ") + e.what());
     }
 }
 
