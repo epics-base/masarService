ac45c44f4afa2af6ce719a662cbb755b7242e32d
Michael Skinner <m.skinner5278@gmail.com>
2016-08-01 11:01:24 -0700

    Fixed a path being called improperly, attempted to add better exit handling for setupTests

--- test/python/setupTests.py
+++ test/python/setupTests.py
@@ -2,11 +2,13 @@ import os
 import threading
 import sys
 import time
-global kill
+import signal
 conf = "../../python/masarserver/masarservice.conf"
-iocexecutable = "/usr/lib/epics/bin/" + os.environ["EPICS_HOST_ARCH"] + "/softIoc"
+if os.environ["EPICS_BASE"]!=None:
+    iocexecutable = os.environ["EPICS_BASE"] + "/bin/" + os.environ["EPICS_HOST_ARCH"] + "/softIoc"
+else:
+    iocexecutable = "/usr/lib/epics/bin/" + os.environ["EPICS_HOST_ARCH"] + "/softIoc"
 executable = "../../cpp/bin/" + os.environ["EPICS_HOST_ARCH"] + "/masarServiceRun"
-kill = 0
 
 def setConfMongo():
     f = open(conf, "rw+")
@@ -23,7 +25,7 @@ def startSQLiteService():
     pid, fd = os.forkpty()
     if pid == 0:
         os.execv(executable, ['masarServiceRun', 'sqliteMasarTestService'])
-    return fd
+    return pid, fd
 
 def startMongoService():
     #conf needs to be set
@@ -31,7 +33,7 @@ def startMongoService():
     pid, fd = os.forkpty()
     if pid == 0:
         os.execv(executable, ['masarServiceRun', 'mongoMasarTestService'])
-    return fd
+    return pid, fd
 
 def startIOC():
     # conf needs to be set
@@ -39,28 +41,35 @@ def startIOC():
     if pid == 0:
         os.chdir("../v3IOC/")
         os.execv(iocexecutable, ['softIoc', '../v3IOC/st.cmd'])
-    return fd
+    return pid, fd
 
 def readfd(fd):
-    while kill == 0:
-        print str(os.read(fd, 1024).strip("\n"))
-    print "done"
+    while 1:
+        print str(os.read(fd, 16384).strip("\n"))
 
 def main():
     global kill
-    kill = 0
     os.chdir(os.path.dirname(os.path.abspath(sys.argv[0])))  # Uses a filename, not good, also only works on linux?
-    iocfd = startIOC()
+    iocpid, iocfd = startIOC()
     iocthread = threading.Thread(group=None, target=readfd, args=(iocfd,), name="iocthread", kwargs={})
     iocthread.start()
 
-    sqlfd = startSQLiteService()
+    sqlpid, sqlfd = startSQLiteService()
     sqlthread = threading.Thread(group=None, target=readfd, args=(sqlfd,), name="sqlthread", kwargs={})
     sqlthread.start()
     time.sleep(3)
-    mongofd = startMongoService()
+    mongopid, mongofd = startMongoService()
     mongothread = threading.Thread(group=None, target=readfd, args=(mongofd,), name="mongothread", kwargs={})
     mongothread.start()
+
+    try:
+        while 1:
+            pass
+    except KeyboardInterrupt:
+        os.kill(sqlpid, signal.SIGKILL)
+        os.kill(mongopid, signal.SIGKILL)
+        os.kill(iocpid, signal.SIGKILL)
+        sys.exit()
 
 if __name__ == '__main__':
     main()
